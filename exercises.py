8caa8360f99274e65f98c0e4b92ca120055b847b
sreyeg <santiagoreyes.g@gmail.com>
2025-01-11 16:04:51 -0800

    Define print_board and print_message methods

--- exercises.py
+++ exercises.py
@@ -13,6 +13,26 @@ class Game():
         self.board = board
     def play_game(self):
         print("Welcome to tic-tac-toe, let's play it")
+    def print_board(self):
+        b = self.board
+        print(f"""
+                A   B   C
+            1)  {b['a1'] or ' '} | {b['b1'] or ' '} | {b['c1'] or ' '}
+                ----------
+            2)  {b['a2'] or ' '} | {b['b2'] or ' '} | {b['c2'] or ' '}
+                ----------
+            3)  {b['a3'] or ' '} | {b['b3'] or ' '} | {b['c3'] or ' '}
+        """)
+
+    def print_message(self):
+        if self.tie:
+            print("tie game!")
+        elif self.winner:
+            print(f"{self.winner} wins the game")
+        else:
+            print(f"It' player {self.turn}'s turn!")
+
+
 
 game_instance = Game()
 game_instance.play_game()
